88220c5f32855f525bd527f0b521aa34bad44bd0
Roman Karavia <3082173+rkaravia@users.noreply.github.com>
2019-12-14 12:50:35 +0100

    Fix eslint issues, add travis config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import serve from "rollup-plugin-serve";
-import { terser } from "rollup-plugin-terser";
-import buble from "rollup-plugin-buble";
-import pkg from "./package.json";
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+import buble from 'rollup-plugin-buble';
+import pkg from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -17,28 +17,32 @@ const banner = `/*!
 `;
 
 export default {
-  input: "src/index.js",
-  external: ["leaflet"],
+  input: 'src/index.js',
+  external: ['leaflet'],
   output: [
     {
       file: pkg.browser,
-      format: "umd",
-      name: "L.TileLayer.Swiss",
-      globals: { leaflet: "L" },
+      format: 'umd',
+      name: 'L.TileLayer.Swiss',
+      globals: { leaflet: 'L' },
       banner,
-      sourcemap: true
+      sourcemap: true,
     },
-    { file: pkg.main, format: "cjs", banner, sourcemap: true },
-    { file: pkg.module, format: "es", banner, sourcemap: true }
+    {
+      file: pkg.main, format: 'cjs', banner, sourcemap: true,
+    },
+    {
+      file: pkg.module, format: 'es', banner, sourcemap: true,
+    },
   ],
   plugins: [
     resolve(),
     commonjs(),
 
     !production && serve(),
-    !production && livereload("dist"),
+    !production && livereload('dist'),
 
     production && buble(),
-    production && terser()
-  ]
+    production && terser(),
+  ],
 };
