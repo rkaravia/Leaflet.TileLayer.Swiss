28379a4a7096cf47bfbeb7c66cfbf2823b76dfd1
Roman Karavia <3082173+rkaravia@users.noreply.github.com>
2025-06-11 14:41:29 +0200

    Update Leaflet version in examples / peerDependency to 1.9.4

--- docs/layers.js
+++ docs/layers.js
@@ -179,13 +179,13 @@ function editOnCodepen(layerId) {
 
   var codepenOptions = {
     css: css,
-    css_external: 'https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/leaflet.css',
+    css_external: 'https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.css',
     editors: '001',
     layout: 'left',
     html: '<div id="mapid"></div>',
     js: js,
     js_external: [
-      'https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/leaflet.js',
+      'https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.js',
       'https://cdn.jsdelivr.net/npm/leaflet-tilelayer-swiss@2.3.0/dist/Leaflet.TileLayer.Swiss.umd.js'
     ].join(';')
   };
